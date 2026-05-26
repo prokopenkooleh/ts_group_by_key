3663ffcb36f1eaabe07b8577518983c716333fd8
Oleh Prokopenko <olehprokopenko1@gmail.com>
2026-05-26 23:07:52 +0300

    add tusk solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,6 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  return Object.groupBy(items, (item) => String(item[key])) as GroupsMap<T>;
 }
